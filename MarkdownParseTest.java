aa123717497860594edc066b54039b7588c28531
ayaooooooooooo <59346123+a1yao@users.noreply.github.com>
2022-05-22 23:27:32 -0700

    Added Lab 4 tests

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -20,5 +20,19 @@ public class MarkdownParseTest {
     //     assertEquals(List.of(), MarkdownParse.getLinks(content));
     // }
 
+    @Test public void labTest1() throws IOException {
+        String content = "`[a link`](url.com)\n\n[another link](`google.com)`\n\n[`cod[e`](google.com)\n\n[`code]`](ucsd.edu)";
+        assertEquals(List.of("`google.com"), MarkdownParse.getLinks(content));
+    }
+
+    @Test public void labTest2() throws IOException {
+        String content = "[a [nested link](a.com)](b.com)\n\n[a nested parenthesized url](a.com(()))\n\n[some escaped \\[ brackets \\]](example.com)";
+        assertEquals(List.of("a.com(()))", "example.com"), MarkdownParse.getLinks(content));
+    }
+
+    @Test public void labTest3() throws IOException {
+        String content = Files.readString(Path.of("C:/Users/antho/OneDrive/Documents/GitHub/markdown-parser/labTest3.md"));
+        assertEquals(List.of("https://sites.google.com/eng.ucsd.edu/cse-15l-spring-2022/schedule"), MarkdownParse.getLinks(content));
+    }
 
 }
